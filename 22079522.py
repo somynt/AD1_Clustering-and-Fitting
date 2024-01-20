624d65e1c836373c905a2d70ae9e8c9de1a0b090
somynt <somynt@gmail.com>
2024-01-20 01:45:13 +0000

    Update 22079522.py

--- 22079522.py
+++ 22079522.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 15 11:40:11 2024
+Created on Wed Jan 17 19:18:22 2024
 
 @author: LENOVO
 """
-
 
 """
 The aim of this program is to analyse the electricity usage per capits (KWH)
@@ -19,7 +18,13 @@ import matplotlib.patches as patches
 from pandas import DataFrame
 from scipy.spatial.distance import cdist
 from sklearn import cluster 
-
+#Loading the required modules
+ 
+from scipy.spatial.distance import cdist 
+from sklearn.datasets import load_digits
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
 
 def ReadWorldBankData (*args):  # Function definition
     '''
@@ -27,14 +32,12 @@ Takes country as first parameter and multiple parameters for climate \
 change indicators.Provide the id code from World bank data as second \
 set of arguments
     '''
-    Read_series_country = wb.data.DataFrame(
-        *args)  # Getting data from Worldbank data set
+    Read_series_country = wb.data.DataFrame(*args)  # Getting data from Worldbank data set
     Read_series_country.index.name = None
     Read_series_country.transform(np.sort, axis=1)
-    # Transpose of the downloaded data set
+    #Transpose of the downloaded data set
     Read_series_country_Transpose = Read_series_country.transpose()
-    dfRead_series_country_Transpose = pd.DataFrame(
-        Read_series_country_Transpose)
+    dfRead_series_country_Transpose = pd.DataFrame(Read_series_country_Transpose)
     return dfRead_series_country_Transpose
 
 
@@ -51,8 +54,8 @@ This section loads all the data from World bank data set using functions
 
 WBData_Ele_HIC = ReadWorldBankData('EG.USE.ELEC.KH.PC', wb.income.members('HIC'))
 WBData_Ele_LIC = ReadWorldBankData('EG.USE.ELEC.KH.PC', wb.income.members('LIC'))
-
-
+WBDataP_GDP_HIC = ReadWorldBankData('NY.GDP.PCAP.CD', wb.income.members('HIC'))
+WBData_GDP_LIC = ReadWorldBankData('NY.GDP.PCAP.CD', wb.income.members('LIC'))
 
 
 """
@@ -61,108 +64,148 @@ In the following section data is cleaned before setting the dataframe for analys
 """
 First the high income countries are cleaned (HIC)
 """
-WBData_Ele_HIC.index.name ='Year' # Give a name Year to the index, since it is year column by default
-WBData_Ele_HIC_Clean_R = WBData_Ele_HIC.dropna(how='all', axis=0, inplace=False)
-WBData_Ele_HIC_Clean_C = WBData_Ele_HIC_Clean_R.dropna(how='all', axis=1, inplace=False)
 
+def Countryset_Cleaned (df):
+     
+    """
+    This function cleans the dataframe along rows and columns
 
-#print(WBData_Ele_HIC)
-#print(WBData_Ele_HIC_Clean_R)
-#print(WBData_Ele_HIC_Clean_C)
-
-
-"""
-Same steps repeated for low income countries data 
-
-"""
-WBData_Ele_LIC.index.name ='Year' # Give a name Year to the index, since it is year column by default
-WBData_Ele_LIC_Clean_R = WBData_Ele_LIC.dropna(how='all', axis=0, inplace=False)
-WBData_Ele_LIC_Clean_C = WBData_Ele_LIC_Clean_R.dropna(how='all', axis=1, inplace=False)
-
-
-#print(WBData_Ele_LIC)
-#print(WBData_Ele_LIC_Clean_R)
-#print(WBData_Ele_LIC_Clean_C)
-"""
-HIC - After cleaning out of 63 rows X 82 Columns only 55 rows were left with 'axis=0' and reduced to 55 rows X 52 columns at axis =1.
-LIC- After cleanng we are left with 44 rows X 11 columns
-In the following section both datasets are trasposed to reveal the country name in the index
-"""
-
-WBData_Ele_HIC_Clean_C = WBData_Ele_HIC_Clean_C.reset_index() # Index reset to use the Year column
-WBData_Ele_HIC_Clean_Countries= WBData_Ele_HIC_Clean_C.set_index('Year').T.rename_axis('Countries').rename_axis(None,axis=1)
-
-#print(WBData_Ele_HIC_Clean_Countries.head())
-
-WBData_Ele_LIC_Clean_C = WBData_Ele_LIC_Clean_C.reset_index() # Index reset to use the Year column
-WBData_Ele_LIC_Clean_Countries= WBData_Ele_LIC_Clean_C.set_index('Year').T.rename_axis('Countries').rename_axis(None,axis=1)
-
-#print(WBData_Ele_LIC_Clean_Countries.head())
-
+    """
+    df.index.name ='Year' # Give a name Year to the index, since it is year column by default
+    df_Clean_R = df.dropna(how='all', axis=0, inplace=False)
+    df_Clean_C = df_Clean_R.dropna(how='all', axis=1, inplace=False)
+    df_Clean_C = df_Clean_C.dropna(how='any', axis=0, inplace=False)
+    
+    return df_Clean_C
 
 """
-The above step revelaed need for further cleaning since some of the data are missing for certain years
-This will only be carried out after filtering required countries from both HIC and LIC.
+Calling the Countryset_cleaned to clean the downloaded dataset
+It accepts a dataframe as argument and returns a dataframe.
 """
+HIC_C = Countryset_Cleaned(WBData_Ele_HIC)
+LIC_C = Countryset_Cleaned(WBData_Ele_HIC)
+#HIC_GDP_C = Countryset_Cleaned(WBDataP_GDP_HIC)
+#LIC_GDP_C = Countryset_Cleaned(WBData_GDP_LIC)
+
+#Combined_df = pd.merge(HIC_C, LIC_C, on='Year') # Merging similar type datasets with temperature
+#Combined_df_GDP = pd.merge(HIC_GDP_C, LIC_GDP_C, on = 'Year')# Merging similar type datasets with GDP
+
+def TransposeDF (df):
+    """
+    This function traspose the given data set and rename the columns (eg:countries)
+    It accepts a dataframe as argument and returns a dataframe.
+    """
+    
+    df.reset_index()
+    df_T= df.T.rename_axis('Countries').rename_axis(columns =None).reset_index()
+    df_T = df_T.set_index('Countries')
+
+    return df_T
+
+rr = TransposeDF(HIC_C)
 
 
 
-"""
-Year_DF_ResetIndex = WBDataPop_Transpose.reset_index() # Index reset to use the Year column
-Countries_DF= Year_DF_ResetIndex.set_index('Year').T.rename_axis('Countries').rename_axis(None,axis=1)
+#Combined_df_T = TransposeDF (Combined_df)
+#Combined_df_T['AverageElecperCapita'] = round(Combined_df_T.mean(axis=1),2)
+#Combined_df_T_Average = Combined_df_T.drop(Combined_df_T.loc[:,"YR1990":"YR2014"], axis=1)
 
-WBDataPop_Transpose.index.name = 'Year'
-"""
+#
 
-"""
-I = WBDataPop_Transpose #Indexed with name 'Year'
-Year_DF_ResetIndex = WBDataPop_Transpose.reset_index() # Index reset to use the Year column
-Countries_DF= Year_DF_ResetIndex.set_index('Year').T.rename_axis('Countries').rename_axis(None,axis=1) # Year_DF transposed to create \
-"""
-""" 
-new dataframe country_Df. This replaces the year in index and now country is the index.
-"""
-#Countries_DF_ResetIndex = Countries_DF.reset_index() # Reset the 
+#sns.lmplot(x = HIC_C.index, y = 'AverageElecperCapita', data = HIC_C)
+#print (RI.head())
 
-#Countries_DF ['Mean'] = Countries_DF.mean(axis=1)
-#Countries_DF_Sorted = Countries_DF.sort_values(by='Mean', ascending=False)
-"""
-row_labels = Countries_DF_Sorted.index.tolist()
-print(row_labels)
-"""
-#Countries_DF_New = Countries_DF_Sorted.filter(['USA', 'JPN', 'DEU', 'GBR', 'FRA', 'ITA', 'KOR', 'ESP', 'POL', 'CAN'], axis=0)
-#Countries_DF_New = Countries_DF_New.drop('Mean', axis=1)
+#sns.lmplot(x = 'Countries', y = 'Year', markers = ["s", "x"], palette = "Set2",data = Combined_df)
 
-"""
-Drop both row and columns based on missing values. 
-This returned same number of columns since there is no missing values 
-"""
-#Countries_DF_New_Clean = Countries_DF_New.dropna(how='any', axis=0, inplace=False)
-"""
+#Combined_df_GDP_T = TransposeDF(Combined_df_GDP)
+#Combined_df_GDP_T_R= Combined_df_GDP_T.dropna(how='all', axis=1, inplace=False)
+#Combined_df_GDP_T= Combined_df_GDP_T_R.dropna(how='any', axis=0, inplace=False)
+
+#Combined_df_T['AverageElecperCapita'] = Combined_df_T.mean(axis=1)
+#Combined_df_GDP_T['AverageGDP'] = Combined_df_GDP_T.mean(axis=1)
 
 
+#Combined_df_T_Average = Combined_df_T.drop(Combined_df_T.loc[:,"YR1960":"YR2014"], axis=1)
+#Combined_df_GDP_T_Average = Combined_df_GDP_T.drop(Combined_df_GDP_T.loc[:,"YR1960":"YR2022"], axis=1)
+
+#Combine_Average_both = pd.merge(Combined_df_T_Average,Combined_df_GDP_T_Average, on= 'Countries' )
+
+#print(Combined_df_T_Average.head())
+#print(Combined_df_GDP_T_Average.head())
+
+#print(Combined_df_T)
+
+scaled_df = StandardScaler().fit_transform(rr)
 
 """
+#===========================
+kmeans_kwargs = {
+"init": "random",
+"n_init": 10,
+"random_state": 1,
+}
+
+#create list to hold SSE values for each k
+sse = []
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+    kmeans.fit(scaled_df)
+    sse.append(kmeans.inertia_)
+
+#visualize results
+plt.plot(range(1, 11), sse)
+plt.xticks(range(1, 11))
+plt.xlabel("Number of Clusters")
+plt.ylabel("SSE")
+plt.show()
+#========================================
+"""
+"""
 """
 
-Clustering Preparation: Initially scale each value to mean 0 and standard deviation 1
-"""
-"""
+#ORIGINAL
 
 
+#Initialize the class object
 
-#print(Countries_DF_New_Clean.head())
-"""
-"""
-WBDataPop_Transpose_sorted = (WBDataPop_Transpose.sort_values(by=['YR1960', 'YR1961', 'YR1962', 'YR1963', 'YR1964', 'YR1965', 'YR1966',
-                                                                  'YR1967', 'YR1968', 'YR1969', 'YR1970', 'YR1971', 'YR1972', 'YR1973',
-                                                                  'YR1974', 'YR1975', 'YR1976', 'YR1977', 'YR1978', 'YR1979', 'YR1980',
-                                                                  'YR1981', 'YR1982', 'YR1983', 'YR1984', 'YR1985', 'YR1986', 'YR1987',
-                                                                  'YR1988', 'YR1989', 'YR1990', 'YR1991', 'YR1992', 'YR1993', 'YR1994',
-                                                                  'YR1995', 'YR1996', 'YR1997', 'YR1998', 'YR1999', 'YR2000','YR2001',
-                                                                  'YR2002', 'YR2003', 'YR2004', 'YR2005', 'YR2006', 'YR2007', 
-                                                                  'YR2008', 'YR2009', 'YR2010', 'YR2011', 'YR2012', 'YR2013', 'YR2014', 
-                                                                  'YR2015', 'YR2016', 'YR2017',
-                                                                'YR2018', 'YR2019'], ascending=False, axis=1))
+kmeans = KMeans(n_clusters= 4)
+ 
+#predict the labels of clusters.
+label = kmeans.fit_predict(scaled_df)
 
-"""
+
+#Getting unique labels
+
+centroids = kmeans.cluster_centers_
+u_labels = np.unique(label)
+
+def create_dataframe(centroids):
+    """
+    Create a DataFrame from centroids.
+
+    Parameters:
+    - centroids: 2D NumPy array representing the centroids.
+
+    Returns:
+    - DataFrame with columns 'C1', 'C2', 'C3', 'C4' etc..
+    """
+    columns = [f'C{i+1}' for i in range(centroids.shape[1])]
+    centroids_df = pd.DataFrame({col: centroids[:, i] for i, col in enumerate(columns)})
+    return centroids_df
+
+centroidsdf = create_dataframe(centroids)
+scaled_df_df = create_dataframe (scaled_df)
+#labeldf = create_dataframe (label)
+labeldf = pd.DataFrame(data=label, columns = ['Clusters'])
+print(labeldf)
+for i in u_labels:
+    plt.scatter(scaled_df[label == i , 0] , scaled_df[label == i , 1] , label = i)
+plt.scatter(centroids[:,0] , centroids[:,1] , s = 80, color = 'k')
+
+plt.legend()
+plt.show()
+
+
+#Combined_df['Average'] = Combined_df.mean(axis=1)
+#print(Combined_df.head())
+
